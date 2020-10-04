0241eb87fba430edca577f64b016a32168dfcdc1
Quadtree <quadtree@gmail.com>
2020-10-04 00:34:52 -0700

    Updated

--- src/actors/StartDoor.ts
+++ src/actors/StartDoor.ts
@@ -1,11 +1,13 @@
 import {Actor} from "../am/Actor";
-import {AbstractMesh, Scene} from "@babylonjs/core";
+import {AbstractMesh, Scene, Vector3} from "@babylonjs/core";
 import {SceneLoader} from "@babylonjs/core/Loading/sceneLoader";
 
 export class StartDoor extends Actor {
     private mesh:AbstractMesh|null = null;
 
     private static baseMesh:AbstractMesh|null;
+
+    private openAmount = 0;
 
     public static async load(scene:Scene){
         this.baseMesh = (await SceneLoader.ImportMeshAsync(null, './assets/pod_bay_door.glb', '', scene)).meshes[0];
@@ -17,5 +19,14 @@ export class StartDoor extends Actor {
 
         this.mesh = StartDoor.baseMesh!.clone("", null, false)!;
         this.mesh.getChildMeshes().forEach(it => it.isVisible = true);
+    }
+
+
+    update(delta: number) {
+        super.update(delta);
+
+        this.openAmount = Math.min(this.openAmount + delta / 2, 1);
+
+        this.mesh!.rotation = new Vector3(0, this.openAmount * Math.PI / 2, 0);
     }
 }
